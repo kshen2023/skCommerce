87505295dd1ef571b6259c9d487e2dbdb55aef0d
kangShen <kshen@rrc.ca>
2024-07-10 22:54:25 -0500

    all products on front page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root 'products#index'
 
+  resources :products, only: [:index, :show]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
