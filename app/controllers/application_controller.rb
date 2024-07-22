10475845e248035ca496845f07d09c90b000dfa1
kangShen <kshen@rrc.ca>
2024-07-22 15:14:36 -0500

    Make use of a custom flash hash message and use the session hash in some way.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :address, :city, :postal_code, :phone, :province_id])
     devise_parameter_sanitizer.permit(:account_update, keys: [:name, :address, :city, :postal_code, :phone, :province_id])
   end
+
+  def after_sign_in_path_for(resource)
+    session[:name] = resource.name
+    flash[:notice] = "You have signed in successfully."
+    root_path
+  end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module ApplicationHelper
-  # def calculate_taxes(province, total_amount)
-  #   tax = Tax.find_by(province: province)
-  #   gst_amount = total_amount * tax.gst
-  #   pst_amount = total_amount * tax.pst
-  #   hst_amount = total_amount * tax.hst
-  #   { gst: gst_amount, pst: pst_amount, hst: hst_amount }
-  # end
+  def custom_flash_message
+    if flash[:notice] && session[:name]
+      "#{session[:name]}, #{flash[:notice]}"
+    else
+      flash[:notice]
+    end
+  end
 end
