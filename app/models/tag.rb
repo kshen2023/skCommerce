6041def8016a78caf4872077fe58e84ee67376c4
kangShen <kshen@rrc.ca>
2024-07-23 14:40:57 -0500

    test

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,8 @@ class Tag < ApplicationRecord
   has_many :product_tags
   has_many :products, through: :product_tags
 
+
+
   def self.ransackable_attributes(auth_object = nil)
     ["created_at", "id", "id_value", "name", "updated_at"]
   end
